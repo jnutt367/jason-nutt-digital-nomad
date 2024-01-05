b098b74ffdb6bc9509a5fdb6848ea0116b236fb2
Jason <jason@Jasons-iMac.lan>
2024-01-05 15:01:28 -0500

    commit for small title

--- src/app/page.js
+++ src/app/page.js
@@ -47,15 +47,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Project 1: {' '} The WORD
-            <br />
-            of God Risen
+          The WORD of God Risen {' '} 
+           
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            This is an entire bible application built from scratch with Next.js and tailwind.css
+           An entire bible application built from scratch with Next.js and tailwind.css
           </p>
         </a>
 
