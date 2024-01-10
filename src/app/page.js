0b13ee2f2645994d03f875f5b4632fb4727bb662
Jason <jason@Jasons-iMac.lan>
2024-01-09 21:45:07 -0500

    commit for new layout

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,18 @@ export default function Home() {
   return (
     <main className="flex min-h-screen bg-red flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-sans text-sm lg:flex">
+
+      <Image
+              src="/jn-high-resolution-logo-transparent.png"
+              alt="My Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-         Built portfolio by editing &nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
+         Built portfolio by editing
+          src/app/page.js
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
