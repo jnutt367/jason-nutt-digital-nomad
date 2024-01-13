591681db33697ff26d9b83f239fd01f09c853fa1
Jason <jason@Jasons-iMac.lan>
2024-01-13 14:18:25 -0500

    commit for new layout

--- src/app/page.js
+++ src/app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
           className="w-full bg-transparent h-12 dark:drop-shadow-[0_0_0.3rem_gold] grey:invert"
           src="/public/2F09AA69-81E8-4C7C-848B-42F31BA65E96_1_105_c.jpeg"
           alt="Next.js Logo"
-          width={1200}
+          width={700}
           height={500}
           priority
         />
