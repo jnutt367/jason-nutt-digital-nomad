34961b6d82cc8ebe1b007a13d733d9e1f02e68b2
Jason <jason@Jasons-iMac.lan>
2024-01-13 13:57:47 -0500

    commit for new layout

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen bg-red flex-col items-center justify-between p-24">
       
-  <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+  <ul className="flex bg-violet-900 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
      
-      <ul className="flex  bg-violet-900  mb-32">
+      
         <li className="w-1/5 bg-transparent"> 
           Home
         </li>
@@ -33,10 +33,10 @@ export default function Home() {
 <button className="w-1/5 bg-purple-400 h-12">
     Get in Touch
 </button>
-</ul>
 
 
-      </div>
+
+      </ul>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
