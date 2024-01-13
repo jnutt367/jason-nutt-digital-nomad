8ff3b80c621145687cefe5d0db00b10a50768529
Jason <jason@Jasons-iMac.lan>
2024-01-13 13:41:40 -0500

    commit for new layout

--- src/app/page.js
+++ src/app/page.js
@@ -5,35 +5,37 @@ export default function Home() {
     <main className="flex min-h-screen bg-red flex-col items-center justify-between p-24">
       
   <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-      <article>
+     
       <ul className="flex mb-8">
-        <li className="w-1/3 bg-gray-400 h-12"> 
+        <li className="w-1/5 bg-gray-400 h-12"> 
           Home
         </li>
-        <li className="w-1/3 bg-gray-500 h-12">
+        <li className="w-1/5 bg-gray-500 h-12">
          Services
         </li>
-        <li className="w-1/3 bg-gray-400 h-12">
+        <li className="w-1/5 bg-gray-400 h-12">
           Github
         </li>
 
 
         <Image
-          className="w-1/4  px-4 bg-gray-400 h-12 dark:drop-shadow-[0_0_0.3rem_gold] grey:invert"
+          className="w-1/5 bg-gray-400 h-12 dark:drop-shadow-[0_0_0.3rem_gold] grey:invert"
           src="/jn-high-resolution-logo-transparent.png"
           alt="Next.js Logo"
-          width={350}
-          height={150}
+          width={500}
+          height={300}
           priority
         />
 
 
-    </ul>
+   
     
-<button>
+<button className="w-1/5 bg-black-400 h-12">
     Get in Touch
 </button>
-</article>
+</ul>
+
+
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
