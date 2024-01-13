14d5637336a3a9f90b48fe3e8738ed488b10e167
Jason <jason@Jasons-iMac.lan>
2024-01-13 13:52:39 -0500

    commit for new layout

--- src/app/page.js
+++ src/app/page.js
@@ -7,19 +7,19 @@ export default function Home() {
   <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
      
       <ul className="flex mb-32">
-        <li className="w-1/5 bg-gray-400"> 
+        <li className="w-1/5 bg-transparent"> 
           Home
         </li>
-        <li className="w-1/5 bg-gray-500">
+        <li className="w-1/5 bg-transparent">
          Services
         </li>
-        <li className="w-1/5 bg-gray-400">
+        <li className="w-1/5 bg-transparent">
           Github
         </li>
 
 
         <Image
-          className="w-1/5 bg-gray-400 h-12 dark:drop-shadow-[0_0_0.3rem_gold] grey:invert"
+          className="w-1/5 bg-transparent h-12 dark:drop-shadow-[0_0_0.3rem_gold] grey:invert"
           src="/jn-high-resolution-logo-transparent.png"
           alt="Next.js Logo"
           width={500}
