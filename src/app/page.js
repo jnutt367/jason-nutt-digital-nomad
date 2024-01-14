293a57f81fdf8a63de8cb133d66e910c0be05e20
Jason <jason@Jasons-iMac.lan>
2024-01-14 09:51:30 -0500

    commit for new layout

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main /* className="flex min-h-screen bg-red flex-col items-center justify-between p-24 pt-2" */>
+    <main className="flex min-h-screen bg-red flex-col items-center justify-between p-24 pt-2">
       
       <Image
           className="bg-transparent h-18 dark:drop-shadow-[0_0_0.3rem_purple] grey:invert"
@@ -50,7 +50,8 @@ export default function Home() {
 
 
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center items-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+     
         <a
           href="https://thewordofgodrisen.vercel.app/"
           className="group rounded-lg border border-gold px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
